b4bc3d42d94feb2131ebdd7fd1362ccf30beb599
Craig Sapp <craigsapp@gmail.com>
2019-04-22 12:14:48 -0700

    Added roll2mstick tool.

---
+++ tools/roll2mstick.cpp
@@ -0,0 +1,80 @@
+//
+// Programmer:    Craig Stuart Sapp <craig@ccrma.stanford.edu>
+// Creation Date: Mon Apr 22 11:58:40 PDT 2019
+// Last Modified: Mon Apr 22 11:58:48 PDT 2019
+// Filename:      midiroll/tools/roll2mstick.cpp
+// Syntax:        C++11
+// vim:           ts=3
+//
+// Description:   Convert roll with tempo changes (every foot) into ticks that represent
+//                milliseconds.  Tempo 60 kept in track 0 to ensure this.
+//
+
+#include "Options.h"
+#include "MidiRoll.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+using namespace smf;
+
+// function declarations:
+void    processMidiFile    (MidiRoll& rollfile, Options& options);
+
+
+///////////////////////////////////////////////////////////////////////////
+
+int main(int argc, char** argv) {
+	Options options;
+	options.process(argc, argv);
+	if ((options.getArgCount() > 2) || (options.getArgCount() == 1)) {
+		cerr << "Usage: " << options.getCommand() << " input.mid output.mid" << endl;
+		exit(1);
+	}
+	MidiRoll midiroll;
+	if (options.getArgCount() == 0) {
+		midiroll.read(cin);
+		processMidiFile(midiroll, options);
+		cout << midiroll << endl;
+	} else {
+		midiroll.read(options.getArg(1));
+		processMidiFile(midiroll, options);
+		midiroll.write(options.getArg(2));
+	}
+	return 0;
+}
+
+
+///////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////
+//
+// processMidiFile --
+//
+
+void processMidiFile(MidiRoll& rollfile, Options& options) {
+	rollfile.joinTracks();
+	rollfile.doTimeAnalysis();
+	rollfile.setMillisecondTicks();
+	rollfile.setTicksPerQuarterNote(1000);
+	bool deletetempo = false;
+
+	for (int i=0; i<rollfile[0].getEventCount(); i++) {
+		MidiEvent* me = &rollfile[0][i];
+		me->tick = (int)(me->seconds * 1000.0 + 0.5);
+		if (me->isTempo()) {
+			if (deletetempo) {
+				me->clear();
+			} else {
+				deletetempo = true;
+			}
+		}
+	}
+
+	rollfile.splitTracks();
+	rollfile.removeEmpties();
+	rollfile.sortTracks();  // maybe not needed
+}
+
+
+
